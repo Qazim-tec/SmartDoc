899e8a5163a6672db777ea3dde2a863520a5d906
DAUDU QAZIM <yetkemsupper@gmail.com>
2025-06-30 20:57:16 +0100

    render

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,3 @@
 // src/config/config.ts
-export const API_BASE_URL = "https://localhost:7151";
+// export const API_BASE_URL = "https://localhost:7151";
+export const API_BASE_URL = "https://smartdoctorai.onrender.com";
